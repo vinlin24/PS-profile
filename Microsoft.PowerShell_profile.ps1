8d46a7cde7cb28868729e193f6d09b1a65c26418
Vincent Lin <soulace2425@outlook.com>
2022-09-19 10:04:56 -0700

    Add aliases for each Python interpreter version

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -327,6 +327,15 @@ function Edit-PreviousCommit {
 
 Set-Alias -Name "amend" -Value "Edit-PreviousCommit"
 
+<# Alias for using specific Python interpreter instead of Python on PATH #>
+$PYTHON_DIR = "$env:LOCALAPPDATA\Programs\Python"
+# Get the version part of the directory, like "39" for Python 3.9
+$PYTHON_VERS = @(Get-ChildItem $PYTHON_DIR | ForEach-Object { $_.Name -replace "Python", "" })
+# Example: py39 for Python 3.9 interpreter
+foreach ($ver in $PYTHON_VERS) {
+    Set-Alias -Name "py$ver" -Value "$PYTHON_DIR\Python$ver\python.exe"
+}
+
 # No welcome text please
 Clear-Host
 Write-Host (Get-Location).ToString()
